dd297d3626d53946195e91763e6db38f0f9591ab
Galip Tolga Erdem <galiperdem@gmail.com>
2020-11-14 19:59:08 +0300

    userclicked

--- js/main.js
+++ js/main.js
@@ -1,21 +1,9 @@
 const aiChoicePool = ["Rock", "Paper", "Scissors"]
+function userClicked(params) {
+    console.log("You choose "+params)
+    const randomNumber = Math.floor(Math.random() * 3)
+    console.log("AI chooses:" + aiChoicePool[randomNumber])
+    document.getElementById("ai_choice_span").innerHTML = aiChoicePool[randomNumber];
+    
 
-
-function rockClicked() {
-  console.log("You choose 'Rock'")
-  const randomNumber = Math.floor(Math.random() * 3)
-  console.log("AI chooses:" + aiChoicePool[randomNumber])
-  document.getElementById("ai_choice_span").innerHTML = aiChoicePool[randomNumber];
-}
-function paperClicked() {
-  console.log("You choose 'Paper'")
-  const randomNumber = Math.floor(Math.random() * 3)
-  console.log("AI chooses:" + aiChoicePool[randomNumber])
-  document.getElementById("ai_choice_span").innerHTML = aiChoicePool[randomNumber];
-}
-function scissorsClicked() {
-  console.log("You choose 'Scissors'")
-  const randomNumber = Math.floor(Math.random() * 3)
-  console.log("AI chooses:" + aiChoicePool[randomNumber])
-  document.getElementById("ai_choice_span").innerHTML = aiChoicePool[randomNumber];
 }
