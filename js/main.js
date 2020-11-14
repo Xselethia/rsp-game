3eaa36b92574825b2262cf62a483a0237e01eb8c
Xselethia <basakavdanerdem@gmail.com>
2020-11-14 20:20:48 +0300

    implemented comparison

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 const aiChoicePool = ["Rock", "Paper", "Scissors"]
 function userClicked(params) {
-    console.log("You choose "+params)
-    const randomNumber = Math.floor(Math.random() * 3)
-    console.log("AI chooses:" + aiChoicePool[randomNumber])
-    document.getElementById("ai_choice_span").innerHTML = aiChoicePool[randomNumber];
-    
-
+  console.log("You choose " + params)
+  const randomNumber = Math.floor(Math.random() * 3)
+  console.log("AI chooses:" + aiChoicePool[randomNumber])
+  document.getElementById("ai_choice_span").innerHTML = aiChoicePool[randomNumber];
+  const result = compareResults(params, aiChoicePool[randomNumber])
+  document.getElementById("result_span").innerHTML = result;
+}
+function compareResults(userData, aiData) {
+  if (userData === aiData) {
+    console.log("Draw!")
+    return "Draw"
+  }
+  // if (userData)
 }
