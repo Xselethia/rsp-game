1b541c1615a4c5abca57b4a57789cf34a843e337
Xselethia <basakavdanerdem@gmail.com>
2020-11-18 00:02:30 +0300

    updated flow

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
+const results = [{
+    text: "DRAW!",
+    cssClass: "fl-draw"
+  },
+  {
+    text: "AI WINS!",
+    cssClass: "fl-ai-wins"
+  }, {
+    text: "YOU WIN!",
+    cssClass: "fl-user-wins"
+  }
+]
 const choices = [{
     id: 1,
     animationClass: "fl-rock",
     text: "Rock",
     compare: function compare(item) {
       if (item.text === this.text) {
-        return {
-          text: "Draw",
-          animationClass: "fl-draw"
-        }
+        return results[0];
       }
       if (item.text === "Paper") {
-        return item;
+        return results[1];
       }
       if (item.text === "Scissors") {
-        return this;
+        return results[2];
       }
     }
   },
@@ -23,16 +32,13 @@ const choices = [{
     text: "Paper",
     compare: function compare(item) {
       if (item.text === this.text) {
-        return {
-          text: "Draw",
-          animationClass: "fl-draw"
-        }
+        return results[0];
       }
       if (item.text === "Rock") {
-        return this;
+        return results[2];
       }
       if (item.text === "Scissors") {
-        return item;
+        return results[1];
       }
     }
   },
@@ -42,16 +48,13 @@ const choices = [{
     text: "Scissors",
     compare: function compare(item) {
       if (item.text === this.text) {
-        return {
-          text: "Draw",
-          animationClass: "fl-draw"
-        }
+        return results[0];
       }
       if (item.text === "Rock") {
-        return item;
+        return results[1];
       }
       if (item.text === "Paper") {
-        return this;
+        return results[2];
       }
     }
   }
@@ -59,52 +62,19 @@ const choices = [{
 
 function userClicked(selectedId) {
   const userChoice = choices.find(t => t.id === selectedId);
-  console.log("You choose " + selectedId)
 
   //initialize user animations  
   initUserAnimation(userChoice);
 
   // get random choice
   const aiChoice = getRandomAiChoice();
-  // document.getElementById("ai_choice_span").innerHTML = randomChoice;
 
   //initialize ai animations
   initAiAnimation(aiChoice)
 
   //compare choices
   const result = userChoice.compare(aiChoice);
-  //compareResults(userChoice, aiChoice)
-  // setTimeout(() => {
-  //   alert(result)
-  // }, 300);
   handleResult(result)
-}
-
-function compareResults(userData, aiData) {
-  debugger;
-  let aiWin = "You Lose!"
-  const userWin = "You win!";
-  const draw = "Draw!";
-  if (userData === aiData) {
-    console.log("Draw!")
-    return draw;
-  }
-  if (
-    (userData === "Rock" && aiData === "Paper") ||
-    (userData === "Paper" && aiData === "Scissors") ||
-    (userData === "Scissors" && aiData === "Rock")
-  ) {
-    console.log("You lose!")
-    return aiWin;
-  }
-  if (
-    (userData === "Rock" && aiData === "Scissors") ||
-    (userData === "Paper" && aiData === "Rock") ||
-    (userData === "Scissors" && aiData === "Paper")
-  ) {
-    console.log("You win!")
-    return userWin;
-  }
 }
 
 function initUserAnimation(selection) {
@@ -122,19 +92,16 @@ function initAiAnimation(aiChoice) {
 }
 
 function handleResult(input) {
-  debugger;
   document.getElementById("result_span").innerHTML = ""
   document.getElementById("result_span").classList.remove("text-focus-in")
   setTimeout(() => {
-    debugger;
-    document.getElementById("result_span").innerHTML = input;
+    document.getElementById("result_span").innerHTML = input.text;
     document.getElementById("result_span").classList.add("text-focus-in")
   }, 100);
 }
 
 function getRandomAiChoice() {
-  const randomNumber = Math.floor(Math.random() * 3)
-  console.log("AI chooses:" + choices[randomNumber]);
+  const randomNumber = Math.floor(Math.random() * 3);
   const aiChoice = choices[randomNumber];
   return aiChoice;
 }
